f33167bc1c6c1affe25ee65771bd2c12ed1ebcd1
o6mer <o6merliraz@gmail.com>
2022-10-19 23:11:15 +0300

    hover effect

--- client/src/Main/Components/General/NavBar.js
+++ client/src/Main/Components/General/NavBar.js
@@ -1,7 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { AiOutlineSetting } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
 import { UserContext } from "../../../contexts/UserContextProvider";
 import useAuth from "../../../Landing/hooks/useAuth";
 import Menu from "@mui/material/Menu";
@@ -12,8 +10,8 @@ import Settings from "@mui/icons-material/Settings";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Divider from "@mui/material/Divider";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import UserModal from "./UserModal";
+import { Tooltip } from "@mui/material";
 
 const NavBar = () => {
   const { user } = useContext(UserContext);
@@ -21,6 +19,7 @@ const NavBar = () => {
 
   const [anchorEl, setAnchorEl] = useState(null);
   const [openModal, setOpenModal] = useState(false);
+
   const opemMenu = Boolean(anchorEl);
   const handleClickMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -38,21 +37,19 @@ const NavBar = () => {
         <img src="" alt="logo" />
       </Link>
       <div className="flex ml-auto gap-4">
-        <button
-          className="flex  items-center w-min"
-          onClick={handleClickMenu}
-          id="profile-button"
-          aria-controls={opemMenu ? "basic-menu" : undefined}
-          aria-haspopup="true"
-          aria-expanded={opemMenu ? "true" : undefined}
-        >
-          <AccountCircleIcon
-            sx={{ color: "white" }}
-            fontSize="large"
-            className="self-end"
-          />
-          <ArrowDropDownIcon sx={{ color: "white" }} className="slef-start" />
-        </button>
+        <Tooltip title="Your Profile">
+          <button
+            className="flex  items-center w-min text-white hover:text-gray-100"
+            onClick={handleClickMenu}
+            id="profile-button"
+            aria-controls={opemMenu ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={opemMenu ? "true" : undefined}
+          >
+            <AccountCircleIcon sx={{ color: "inherit" }} fontSize="large" />
+            <ArrowDropDownIcon sx={{ color: "inherit" }} />
+          </button>
+        </Tooltip>
         <Menu
           id="basic-menu"
           anchorEl={anchorEl}
