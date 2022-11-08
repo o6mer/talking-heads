220b620d57b0c0747d653a55266ae42113bcd2d5
o6mer <o6merliraz@gmail.com>
2022-11-08 17:16:28 +0200

    Merge branch 'main' into login-signup-pages

--- client/src/Main/ErrorPage.jsx
+++ client/src/Main/ErrorPage.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import NavBar from "./Components/General/NavBar";
 import ErrorImage from "../Media/Moai404.jpg";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   useEffect(() => {
     console.error("Status code: 404. Page not found");
   }, []);
   return (
-    <div className={"items-center flex flex-col justify-center w-full h-full"}>
-      <p>Page not found</p>
-      <img className={"w-96"} alt="page not found" src={ErrorImage} />
+    <div>
+      <NavBar isError={true} />
+      <div className={"items-center flex flex-col mt-20"}>
+        <p>Page not found</p>
+        <img className={"w-96"} src={ErrorImage} />
+        <Link to={`/main`}>Back to chat</Link>
+        <Link to={`/`}>Back to home page</Link>
+      </div>
     </div>
   );
 };
