1b32c6cba6d13fabc2a62a8da54eb3d7859c8699
o6mer <o6merliraz@gmail.com>
2022-11-27 16:22:07 +0200

    landing page mobiel navingation drawer

--- client/src/Landing/LandingPage.js
+++ client/src/Landing/LandingPage.js
@@ -5,6 +5,7 @@ import Hero from "./Components/Home/Hero";
 import HowItWorks from "./Components/Home/HowItWorks";
 import Fade from "react-reveal/Fade";
 import Footer from "./Components/Home/Footer";
+import MobileNav from "./Components/General/MobileNav";
 
 const LandingPage = () => {
   // setPassword(e.target.value);
@@ -12,7 +13,9 @@ const LandingPage = () => {
   return (
     <main className=" h-screen w-screen flex justify-center overflow-x-hidden scroll-smooth">
       <div className="w-full  flex flex-col items-center gap-20 ">
-        <div className={`background-picture flex flex-col justify-center items-center`}>
+        <div
+          className={`background-picture flex flex-col justify-center items-center`}
+        >
           <NavBar />
           <Hero />
         </div>
