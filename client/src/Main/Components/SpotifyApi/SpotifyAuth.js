116f418b44be156f0e1f6486afaa1ed258d2996f
o6mer <o6merliraz@gmail.com>
2023-01-15 19:39:04 +0200

    spotify redirect

--- client/src/Main/Components/SpotifyApi/SpotifyAuth.js
+++ client/src/Main/Components/SpotifyApi/SpotifyAuth.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-const AUTH_URL =
-  "https://accounts.spotify.com/authorize?client_id=d679667fbb3e4d9e92688887dd7e6db3&response_type=code&redirect_uri=http://localhost:3000/main/1&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=d679667fbb3e4d9e92688887dd7e6db3&response_type=code&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URL}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export default function SpotifyAuth() {
   return (
